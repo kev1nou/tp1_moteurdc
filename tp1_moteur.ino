4d5cddcb04c82ee335cb322b88dea7041321b762
Heng <e1869960@cmaisonneuve.qc.ca>
2025-01-31 13:37:56 -0500

    1 tour par sec

--- tp1_moteur.ino
+++ tp1_moteur.ino
@@ -1,9 +1,11 @@
+//Pour faire tourner le moteur à 1 tour/sec
+//Source de tension à 6,21V
 
-int pinPwm = 3;
+int pinPwm = 3; //Pin 3 pour PWM
 
 void setup() {
-  pinMode(pinPwm, OUTPUT);
-  analogWrite(pinPwm, 50);
+  pinMode(pinPwm, OUTPUT); //Mettre en sortie pinPwm
+  analogWrite(pinPwm, 50); //Envoyer un duty cycle de 19,6% 
   delay(100);
 }
 
